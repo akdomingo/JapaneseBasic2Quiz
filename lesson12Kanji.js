6db1968afb440a1c3d89d70211ccebfb23f4bcc0
akdomingo <akdomingo@gmail.com>
2017-06-01 19:24:38 +0800

    Add files via upload

--- lesson12Kanji.js
+++ lesson12Kanji.js
@@ -1,19 +1,285 @@
 (function() {
   var lesson12Kanji = [
     {
+      kanji: '昔',
+      kana: 'むかし',
+      english: ['old times',],
+    },
+    {
+      kanji: '昔々',
+      kana: 'むかしむかし',
+      english: ['once upon a time',],
+    },
+    {
+      kanji: '昔話',
+      kana: 'むかしばなし',
+      english: ['old tale',],
+    },
+    {
+      kanji: '大昔',
+      kana: 'おおむかし',
+      english: ['ancient times',],
+    },
+    {
+      kanji: '人々',
+      kana: 'ひとびと',
+      english: ['people',],
+    },
+    {
+      kanji: '時々',
+      kana: 'ときどき',
+      english: ['sometimes',],
+    },
+    {
+      kanji: '色々な',
+      kana: 'いろいろな',
+      english: ['various',],
+    },
+    {
+      kanji: '色々な所',　//moved from lesson 11
+      kana: 'いろいろなところ',
+      english: ['various places',],
+    },
+    {
+      kanji: '神さま',
+      kana: 'かみさま',
+      english: ['God',],
+    },
+    {
       kanji: '神社', //duplicate removed from lesson 7
       kana: 'ジンジャー',
       english: [ 'shrine',],
     },
     {
-      kanji: '色々な所',　//moved fro lesson 11
-      kana: 'いろいろなところ',
-      english: ['various places',],
+      kanji: '神道',
+      kana: 'しんとう',
+      english: ['Shinto (religion)',],
     },
     {
-      kanji: '',
-      kana: '',
-      english: ['',],
+      kanji: '神戸市',
+      kana: 'こうべし',
+      english: ['Kobe City',],
+    },
+    {
+      kanji: '早い',
+      kana: 'はやい',
+      english: ['early',],
+    },
+    {
+      kanji: '早起きする',
+      kana: 'はやおきする',
+      english: ['to get up early',],
+    },
+    {
+      kanji: '早朝',
+      kana: 'そうちょう',
+      english: ['early morning',],
+    },
+    {
+      kanji: '起きる',
+      kana: 'おきる',
+      english: ['to get up',],
+    },
+    {
+      kanji: '起こす',
+      kana: 'おこす',
+      english: ['to wake someone up',],
+    },
+    {
+      kanji: '起立する',
+      kana: 'きりつする',
+      english: ['to stand up',],
+    },
+    {
+      kanji: '牛',
+      kana: 'うし',
+      english: ['cow',],
+    },
+    {
+      kanji: '牛乳',
+      kana: 'ぎゅうにゅう',
+      english: ['milk',],
+    },
+    {
+      kanji: '牛肉',
+      kana: 'ぎゅうにく',
+      english: ['beef',],
+    },
+    {
+      kanji: '子牛',
+      kana: 'こうし',
+      english: [
+        'calf',
+        ' veal',
+      ],
+    },
+    {
+      kanji: '使う',
+      kana: 'つかう',
+      english: ['to use',],
+    },
+    {
+      kanji: '大使',
+      kana: 'たいし',
+      english: ['ambassador',],
+    },
+    {
+      kanji: '使用中',
+      kana: 'しようちゅう',
+      english: ['"Occupied"',],
+    },
+    {
+      kanji: 'お使い',
+      kana: 'おつかい',
+      english: ['errand',],
+    },
+    {
+      kanji: '働く',
+      kana: 'はたらく',
+      english: ['to work',],
+    },
+    {
+      kanji: '共働き',
+      kana: 'ともばたらき',
+      english: ['both husband and wife working',],
+    },
+    {
+      kanji: '労働',
+      kana: 'ろうどう',
+      english: ['labor',],
+    },
+    {
+      kanji: '連れて帰る',
+      kana: 'つれてかえる',
+      english: ['to bring (a person) back',],
+    },
+    {
+      kanji: '国連',
+      kana: 'こくれん',
+      english: ['United Nations',],
+    },
+    {
+      kanji: '連休',
+      kana: 'れんきゅう',
+      english: ['consecutive holidays',],
+    },
+    {
+      kanji: '別れる',
+      kana: 'わかれる',
+      english: ['to separate',],
+    },
+    {
+      kanji: '別に',
+      kana: 'べつに',
+      english: ['not in particular',],
+    },
+    {
+      kanji: '特別な',
+      kana: 'とくべつな',
+      english: ['special',],
+    },
+    {
+      kanji: '差別',
+      kana: 'さべつ',
+      english: ['discrimination',],
+    },
+    {
+      kanji: '別々に',
+      kana: 'べつべつに',
+      english: ['separately',],
+    },
+    {
+      kanji: '一度',
+      kana: 'いちど',
+      english: ['once',],
+    },
+    {
+      kanji: '今度',
+      kana: 'こんど',
+      english: ['near future',],
+    },
+    {
+      kanji: '温度',
+      kana: 'おんど',
+      english: [
+        'time',
+        ' degrees',
+      ],
+    },
+    {
+      kanji: '三十度',
+      kana: 'さんじゅうど',
+      english: ['30 degrees',],
+    },
+    {
+      kanji: '態度',
+      kana: 'たいど',
+      english: ['attitude',],
+    },
+    {
+      kanji: '赤',
+      kana: 'あか',
+      english: ['red color',],
+    },
+    {
+      kanji: '赤い',
+      kana: 'あかい',
+      english: ['red',],
+    },
+    {
+      kanji: '赤ちゃん',
+      kana: 'あかちゃん',
+      english: ['baby',],
+    },
+    {
+      kanji: '赤道',
+      kana: 'せきどう',
+      english: ['the equator',],
+    },
+    {
+      kanji: '赤十字',
+      kana: 'せきじゅうじ',
+      english: ['the Red Cross',],
+    },
+    {
+      kanji: '青',
+      kana: 'あお',
+      english: ['blue color',],
+    },
+    {
+      kanji: '青い',
+      kana: 'あおい',
+      english: ['blue',],
+    },
+    {
+      kanji: '青年',
+      kana: 'せいねん',
+      english: ['youth',],
+    },
+    {
+      kanji: '青空',
+      kana: 'あおぞら',
+      english: ['blue sky',],
+    },
+    {
+      kanji: '青信号',
+      kana: 'あおしんごう',
+      english: ['green light',],
+    },
+    {
+      kanji: '色',
+      kana: 'いろ',
+      english: ['color',],
+    },
+    {
+      kanji: '景色',
+      kana: 'けしき',
+      english: ['scenery',],
+    },
+    {
+      kanji: '特色',
+      kana: 'とくしょく',
+      english: ['characteristic',],
     },
   ];
 
